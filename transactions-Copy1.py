4264914a08f4f07a8de1f797f5bba8ef3e1509c5
Harry Kalodner <harry.kalodner@gmail.com>
2018-11-06 04:21:59 +0000

    spam distribution, returns unspent outputs of txes

--- transactions-Copy1.py
+++ transactions-Copy1.py
@@ -1,3 +1,5 @@
+"""Module for output of slack scripts"""
+
 import blocksci 
 import numpy as np
 import pandas as pd
@@ -7,35 +9,44 @@ global CHAIN
 CHAIN = blocksci.Blockchain("/home/ubuntu/bitcoin")
 
 def get_spam(start, end):
+    """
+    returns distribution of transactions where fees are below 6 satoshis per byte
     
-    subchain = CHAIN.range(start=start, end=end)
+    start, end: timestamps
+    """
+    
+    subchain = chain.range(start=start, end=end)
     
     transactions = []
     for block in range(len(subchain)):
-        #finds the value of outputs spent in a transaction (ie not the UTXOs)
-        t = [(output.value, time.ctime(int(output.block.timestamp)),output.address)
-             for output in subchain[block].txes.outputs if output.is_spent and output.value < 550]
+        average_fee = subchain[block].fee / len(subchain[block]) 
+        
+        t = [(tx.fee, average_fee,(tx.fee/average_fee)*100,tx.fee_per_byte(),  time.ctime(subchain[block].timestamp)) for tx in subchain[block].txes if tx.fee_per_byte()  <= 6 and tx.fee > 0]
+        if(t): transactions.append(t)
+            
+    transactions = sum(transactions, [])
+    df = pd.DataFrame(transactions, columns=['Tx_Fee', 'Average_Fee','Pcnt_of_average_fee' ,'Fee_per_byte', 'Time'])
+    
+    
+    return df.describe().round(2)
+
+def get_top_transactions(start, end, threshold = 2000):
+    """
+    returns all transactions obove threshold for period. Gives output address (if unspent), value and date
+    
+    start, end: timestamps
+    threshold: btc value
+    """
+    
+    subchain = chain.range(start=start, end=end)
+    
+    transactions = []
+    for block in range(len(subchain)):
+        t = [(output.value/10e7, time.ctime(int(output.block.timestamp)),output.address)
+             for output in subchain[block].txes.outputs if output.value/10e7 > threshold and output.is_spent == False]
         if(t): transactions.append(t)
             
             
     transactions = sum(transactions, [])
-    df = pd.DataFrame(transactions, columns=['Value', 'Date', 'Address'])
-    return len(df)
-
-def get_top_transactions(start, end, threshold = 500):
-    
-    subchain = CHAIN.range(start=start, end=end)
-
-    
-    transactions = []
-    for block in range(len(subchain)):
-        #finds the value of outputs spent in a transaction (ie not the UTXOs)
-        t = [(output.value/10e8, time.ctime(int(output.block.timestamp)),output.address)
-             for output in subchain[block].txes.outputs if output.is_spent and output.value/10e8 > threshold]
-        if(t): transactions.append(t)
-            
-            
-    transactions = sum(transactions, [])
-    df = pd.DataFrame(transactions, columns=['Value', 'Date', 'Address'])
+    df = pd.DataFrame(transactions, columns=['Value', 'Date', 'Output Address'])
     return df
-
